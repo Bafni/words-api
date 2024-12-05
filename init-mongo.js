c22905c975891084703399c99eaa75717706e9e0
Bafni <reclamabafni@gmail.com>
2024-12-05 19:00:16 +0200

    Fix .gitignore issues

--- init-mongo.js
+++ init-mongo.js
@@ -1,5 +1,5 @@
 // init-mongo.js
-db = db.getSiblingDB('vocabulary1');
+db = db.getSiblingDB('vocabulary');
 db.createCollection('words');
 
 print("Database 'vocabulary' and collection 'words' are created if they did not exist.");
