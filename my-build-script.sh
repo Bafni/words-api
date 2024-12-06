8ef7d14c66423778ba36ab442248b62dfabbceb6
Bafni <reclamabafni@gmail.com>
2024-12-06 22:33:18 +0200

    fix: new realise

--- my-build-script.sh
+++ my-build-script.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Задаємо нову версію з параметрів
 VERSION=$1
-
-# Оновлюємо версію у package.json
 jq --arg version "$VERSION" '.version = $version' package.json > temp.json && mv temp.json package.json
